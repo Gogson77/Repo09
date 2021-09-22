618a87d84a09d2a72bc3583d32379d0ba175d29c
Gordan Kranjčić <gordankranjcic@gmail.com>
2021-09-22 09:05:10 +0200

    delimiter updated

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,11 +1,8 @@
 // Only change code below this line
 function logicOrder(num) {
-    if (num < 50) {
-        return "Less than 50";
-    } else if (num < 100) {
-        return "Less than 100"
-    } else {
-        return "Greater than or equal to 100";
+    if (num < 50) { return "Less than 50";
+    } else if (num < 100) { return "Less than 100";
+    } else { return "Greater than or equal to 100";
     }
 }
 console.log(logicOrder(28));
